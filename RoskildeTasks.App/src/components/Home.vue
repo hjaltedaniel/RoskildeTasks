b12e02346e712b67298b0394e4b235b3b1498c30
Hjalte Daniel Hansen <he@hjaltedaniel.io>
2019-11-07 16:25:04 +0100

    Creating menu styling and markup

--- RoskildeTasks.App/src/components/Home.vue
+++ RoskildeTasks.App/src/components/Home.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="home">
-        <h1 class="title">{{ msg }}</h1>
-        <p>Welcome to your new single-page application, built with <a href="https://vuejs.org" target="_blank">Vue.js</a>.</p>
-    </div>
+  <MainMenu />
 </template>
 
 <script>
-    export default {
-        name: 'Home',
-        props: {
-            msg: String
-        }
-    };
+import MainMenu from "@/components/MainMenu.vue";
+export default {
+  name: "Home",
+  components: {
+    MainMenu
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ RoskildeTasks.App/src/components/MainMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <aside class="main-menu column is-1">
+    <div class="header-logo">
+      <img src="../assets/Icons/canopy.svg" alt="RF Tasks" class="header-logo__image" />
+    </div>
+    <ul class="menu-list">
+      <li class="menu-list__item is-active">
+        <img src="../assets/Icons/tasks.svg" alt="Tasks" class="item-icon" />
+        <span class="item-description">Tasks</span>
+      </li>
+      <li class="menu-list__item">
+        <img src="../assets/Icons/comments.svg" alt="Messages" class="item-icon" />
+        <span class="item-description">Messages</span>
+      </li>
+      <li class="menu-list__item">
+        <img src="../assets/Icons/copy.svg" alt="Ressources" class="item-icon" />
+        <span class="item-description">Ressources</span>
+      </li>
+      <li class="menu-list__item">
+        <img src="../assets/Icons/user.svg" alt="Profile" class="item-icon" />
+        <span class="item-description">Profile</span>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
