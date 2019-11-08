395ace6387cad2a0d726d1fdb1996e2f088620aa
Hjalte Daniel Hansen <he@hjaltedaniel.io>
2019-11-08 09:26:04 +0100

    Installing vue router

--- RoskildeTasks.App/src/App.vue
+++ RoskildeTasks.App/src/App.vue
@@ -1,22 +1,12 @@
 <template>
-  <div class="columns">
-    <MainMenu />
-    <ContentArea></ContentArea>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import MainMenu from "@/components/MainMenu.vue";
-import ContentArea from "@/components/ContentArea.vue";
-
-export default {
-  name: "app",
-  components: {
-    MainMenu,
-    ContentArea
-  }
-};
-</script>
 
 <style lang="scss">
 @import "../src/assets/Styles/Site";
