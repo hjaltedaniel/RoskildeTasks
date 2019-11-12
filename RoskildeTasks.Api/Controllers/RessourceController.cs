aa6da11a028e33c39e8300f67629305cd5e1d190
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-11-12 17:16:06 +0100

    Create ressource doctype, controller and class

--- RoskildeTasks.Api/Controllers/RessourceController.cs
+++ RoskildeTasks.Api/Controllers/RessourceController.cs
@@ -9,10 +9,56 @@ using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using Umbraco.Web.WebApi;
 using RoskildeTasks.Api.Models;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace RoskildeTasks.Api.Controllers
 {
     public class RessourceController : UmbracoApiController
     {
+        [RoleAuthorize]
+        [HttpGet]
+        public List<RessourceItem> GetAllRessources()
+        {
+            var currentUser = Members.CurrentUserName;
+            IMemberService ms = Services.MemberService;
+            IContentService cs = Services.ContentService;
+
+            var everyRessource = cs.GetContentOfContentType(1100);
+
+            List<RessourceItem> usersRessources = new List<RessourceItem>();
+
+            foreach (var ressource in everyRessource)
+            {
+                string ressourceGroups = ressource.GetValue("memberAccess").ToString();
+
+                if (Functions.IsMemberInGroups(ressourceGroups, currentUser))
+                {
+                    RessourceItem usersRessource = new RessourceItem();
+                    usersRessource.Name = ressource.Name;
+                    usersRessource.Url = ressource.GetValue("file").ToString();
+                    usersRessource.Filetype = Path.GetExtension(usersRessource.Url).Replace(".", "");
+
+                    var categoryUri = ressource.GetValue("category");
+                    var thisCategory = cs.GetById(Umbraco.TypedContent(categoryUri).Id);
+                    CategoryItem ressourceCategory = new CategoryItem();
+                    ressourceCategory.Id = thisCategory.Id;
+                    ressourceCategory.Name = thisCategory.Name;
+                    ressourceCategory.ShortName = thisCategory.GetValue("shortName").ToString();
+
+                    var colorString = thisCategory.GetValue("categoryColor").ToString();
+                    ColorItem color = JsonConvert.DeserializeObject<ColorItem>(colorString);
+                    ressourceCategory.Color = color;
+
+                    ressourceCategory.StandardMessage = thisCategory.GetValue("standardMessage").ToString();
+                    ressourceCategory.isOnlyMessages = thisCategory.GetValue<bool>("isOnlyMessages");
+                    usersRessource.Category = ressourceCategory;
+
+                    usersRessources.Add(usersRessource);
+                }
+            }
+
+            return usersRessources;
+        }
     }
 }
---
+++ RoskildeTasks.Api/Models/RessourceItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoskildeTasks.Api.Models
+{
+    public class RessourceItem
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Filetype { get; set; }
+        public CategoryItem Category { get; set; }
+    }
+}
