753cd3c4fbb77db92985da28eb8e0d86a71d6fc1
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-11-13 21:10:34 +0100

    Creating Answer view and models

---
+++ RoskildeTasks.Api/Models/AnswerRootItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoskildeTasks.Api.Models
+{
+    public class AnswerRootItem
+    {
+        public int TaskId { get; set; }
+        public List<AnswerItem> Rows { get; set; }
+    }
+}
