2aee4ec629ccd1642259eef908bf198dabb78a65
André Larsen <andrewsondergaard@gmail.com>
2019-11-14 12:08:55 +0100

    Feature: Added ressource categories

--- client/src/views/Resources/Resources.js
+++ client/src/views/Resources/Resources.js
@@ -5,7 +5,12 @@ export default {
   props: [],
   data () {
     return {
-
+      iconColor: 'green',
+      categories: [
+        {title: 'Signs and Layout'},
+        {title: 'Construction'},
+        {title: 'Supply'}
+      ]
     }
   },
   computed: {
