3bf29da0f93b50ce6a661b0af5814669e5721841
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-11-20 14:23:28 +0100

    Creating DTOs for handling messages

---
+++ RoskildeTasks.Api/Models/DTO/AnswerRoot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoskildeTasks.Api.Models.DTO
+{
+    public class AnswerRoot
+    {
+        public int TaskId { get; set; }
+        public List<AnswerItem> Rows { get; set; }
+    }
+}
