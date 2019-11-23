0e129f79a9c72253939a0e8c8d28b62430aee2c1
André Larsen <andrewsondergaard@gmail.com>
2019-11-23 15:50:46 +0100

    Feature: Profile view. Basic display

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -5,7 +5,14 @@ export default {
   props: [],
   data () {
     return {
-
+      user: {
+        name: 'John Doe',
+        company: 'Doe U Even Lift',
+        email: 'DoeLift@muscles.com',
+        password: '*********',
+        status: 'Active',
+        accessGroup: 'Food Stalls - RF2020'
+      }
     }
   },
   computed: {
