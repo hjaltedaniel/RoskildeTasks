a08d0c7dd6b7e5289de4631e8596bd115cf3b35d
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-11-26 00:09:44 +0100

    Changing CORS settings on Umbraco Project

---
+++ RoskildeTasks.Umbraco/MyApplication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Umbraco
+{
+    public class MyApplication : Umbraco.Web.UmbracoApplication
+    {
+        protected override void OnApplicationStarting(object sender, EventArgs e)
+        {
+            base.OnApplicationStarting(sender, e);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var corsAttr = new EnableCorsAttribute("http://localhost:8080", "*", "*");
+            config.EnableCors(corsAttr);
+        }
+    }
+}
