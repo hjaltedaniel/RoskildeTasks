79ffc59bfb3246b35c372c4872cff7431bbfd404
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-11-26 00:10:07 +0100

    Installing axios and creating api service

---
+++ client/src/services/ApiService.js
@@ -0,0 +1,16 @@
+﻿import axios from "axios"
+
+const apiClient = axios.create({
+	baseURL: `https://localhost:44383/umbraco/api`,
+	withCredentials: false,
+	auth: {
+		username: 'mikuna',
+		password: '1234567890'
+	},
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json"
+	}
+})
+
+export default apiClient;
