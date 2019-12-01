48d4659041ffafe4e388a960ebb853f713320569
Carmocanu Eduard <carmocanueduard@gmail.com>
2019-12-01 22:16:41 +0200

    Create Editor table component and sub-components

---
+++ client/src/components/EditorTable/EditorRow/EditorCell/EditorCell.js
@@ -0,0 +1,14 @@
+
+export default {
+	name: 'editor-cell',
+	components: {},
+	props: {
+		cell: {
+			required: true
+		},
+		isEditable: {
+			type: Boolean,
+			required: true
+		}
+	}
+}
---
+++ client/src/components/EditorTable/EditorRow/EditorCell/index.vue
@@ -0,0 +1,4 @@
+<template src="./EditorCell.html"></template>
+<script src="./EditorCell.js"></script>
+<style src="./EditorCell.scss" scoped lang="scss"></style>
+
