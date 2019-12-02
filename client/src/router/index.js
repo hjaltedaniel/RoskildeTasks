1628c04a353b927ed1b2ff6fc535850769f928a3
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-12-02 12:11:30 +0100

    Fixing icon heights in main-menu

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home';
 import Tasks from '../views/Tasks';
 import TaskDetails from '../views/TaskDetails';
 import Messages from '../views/Messages';
@@ -13,7 +12,7 @@ const routes = [
 	{
 		path: '/',
 		name: 'home',
-		component: Home,
+		component: Tasks,
 	},
 	{
 		path: '/tasks',
