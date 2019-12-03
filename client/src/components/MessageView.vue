39901d1cf2e53dcd14baca1fe0256ed20e50a74b
Hjalte Daniel Hansen <hej@hjaltedaniel.io>
2019-12-03 16:22:33 +0100

    Adding breakers to messageview

--- client/src/components/MessageView.vue
+++ client/src/components/MessageView.vue
@@ -1,6 +1,34 @@
 ﻿<template>
 	<div v-if="messages.length != 0" class="d-flex flex-column message-view">
-		<div class="single-message d-flex align-items-center" v-bind:class="{ adminMessage: message.isFromAdmin }" v-for="message in messages" :key="message.Date">
+		<div class="single-message d-flex align-items-center" v-bind:class="{ adminMessage: message.isFromAdmin }" v-for="message in olderMessages" :key="message.Date">
+			<span class="single-message__time">
+				{{getTimeFromDate(message.Date)}}
+			</span>
+			<div class="single-message__content">
+				<span class="content">
+					{{message.Content}}
+				</span>
+			</div>
+		</div>
+		<div class="message-view__breaker d-flex justify-content-center" v-if="yesterdaysMessages != ''">
+			<span class="breaker-line"></span>
+			<span class="breaker-content">Yesterday</span>
+		</div>
+		<div class="single-message d-flex align-items-center" v-bind:class="{ adminMessage: message.isFromAdmin }" v-for="message in yesterdaysMessages" :key="message.Date">
+			<span class="single-message__time">
+				{{getTimeFromDate(message.Date)}}
+			</span>
+			<div class="single-message__content">
+				<span class="content">
+					{{message.Content}}
+				</span>
+			</div>
+		</div>
+		<div class="message-view__breaker d-flex justify-content-center" v-if="todaysMessages != ''">
+			<span class="breaker-line"></span>
+			<span class="breaker-content">Today</span>
+		</div>
+		<div class="single-message d-flex align-items-center" v-bind:class="{ adminMessage: message.isFromAdmin }" v-for="message in todaysMessages" :key="message.Date">
 			<span class="single-message__time">
 				{{getTimeFromDate(message.Date)}}
 			</span>
@@ -30,13 +58,21 @@
 			noMessage: {required: true},
 		},
 		data () {
-			return {}
+			return {
+				todaysMessages: [],
+				yesterdaysMessages: [],
+				olderMessages: []
+			}
 		},
 		computed: {
 		},
 		mounted() {
+			this.groupMessages();
 		},
 		watch: {
+			messages: function () {
+				this.groupMessages();
+			}
 		},
 		methods: {
 			getTimeFromDate(date) {
@@ -60,6 +96,42 @@
 				}
 
 				return hours + ":" + minutes;
+			},
+			groupMessages() {
+				let today = new Date();
+
+				let todaysMessages = [];
+				let yesterdaysMessages = [];
+				let olderMessages = [];
+
+				this.messages.forEach(
+					function (message, index) {
+						const d = new Date(message.Date);
+
+						const diffTime = Math.abs(today - d);
+						const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+						if (returnDate(d) == returnDate(today)) {
+							todaysMessages.push(message)
+						}
+						else if (diffDays == 1 && returnDate(d) != returnDate(today)) {
+							yesterdaysMessages.push(message)
+						}
+
+						else {
+							olderMessages.push(message)
+						}
+
+						function returnDate(date) {
+							var dd = String(date.getDate()).padStart(2, '0');
+							var mm = String(date.getMonth() + 1).padStart(2, '0'); //January is 0!
+							var yyyy = date.getFullYear();
+							return dd + '/' + mm + '/' + yyyy;
+						}
+					})
+				this.olderMessages = olderMessages;
+				this.yesterdaysMessages = yesterdaysMessages;
+				this.todaysMessages = todaysMessages;
 			}
 		}
 }
