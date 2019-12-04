1b9309e077c8ea16cef97736603a65e729dedd43
Hjalte Daniel Hansen <he@hjaltedaniel.io>
2019-12-04 17:35:48 +0100

    Setting first category item active on init

--- client/src/views/Resources/Resources.js
+++ client/src/views/Resources/Resources.js
@@ -1,30 +1,48 @@
 import ResourceTabs from '../../components/ResourceTabs/index.vue';
 
 export default {
-  name: 'resources',
-  components: {
-    ResourceTabs,
-  },
-  props: [],
-  data () {
-    return {
-      
-    }
-  },
+	name: 'resources',
+	components: {
+		ResourceTabs,
+	},
+	props: [],
+	data() {
+		return {
+			categoriesList: []
+		}
+	},
 	computed: {
-		categoriesList() {
+		storeCategories() {
 			return this.$store.state.categoriesList;
-		},
-	  ressourcesList() {
-		  return this.$store.getters.filterRessourcesByCategory(this.categoriesList);
-	  },
-  },
-  mounted () {
+		}
+	},
+	watch: {
+		storeCategories: function () {
+			this.setCategories();
+		}
+	},
+	mounted() {
+		this.setCategories();
+	},
+	methods: {
+		setCategories() {
+			let categories = this.storeCategories;
+			let catList = [];
 
-  },
-  methods: {
-    
-  }
+			if (categories != undefined) {
+				categories.forEach(function (item, index) {
+					if (item.isOnlyMessages == false) {
+						catList.push(item);
+					}
+				})
+				if (this.$route.params.resourcelist == undefined) {
+					this.$router.push({
+						path: `/resources/${catList[0].Id}`
+					})
+				}
+
+				this.categoriesList = catList;
+			}
+		}
+	}
 }
-
-
